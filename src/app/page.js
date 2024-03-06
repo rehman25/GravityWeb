d4cce0250bc048cee978a1b08ce536607fa3be99
0318119 <professionalalexwilson@gmail.com>
2024-03-05 16:30:14 -0800

    hamzA

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
 import React from 'react'
+// import NoSSR from 'react-no-ssr';
+import style from './styles/home.module.css'
+
 
 const page = () => {
   return (
-    <div>page</div>
+    // <NoSSR>
+      <section>
+        <div>
+          <video controls width="100%">
+            <source src="https://player.vimeo.com/video/771916988?h=d7bec5dce4&background=1" />
+            {/* Add additional source tags for different video formats if needed */}
+          </video>
+        </div>
+      </section>
+    // </NoSSR>
   )
 }
 
