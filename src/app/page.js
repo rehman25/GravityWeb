2a3eadaa2754a1012a90bfe832c8aab87d2d56b4
0318119 <professionalalexwilson@gmail.com>
2024-03-05 18:10:43 -0800

    some changes by hamza waseem

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,41 @@
 import React from 'react'
 // import NoSSR from 'react-no-ssr';
 import style from './styles/home.module.css'
+import Image from 'next/image';
+import Button from './components/buttons/index';
+
 
 
 const page = () => {
+
   return (
-    // <NoSSR>
+    <>
       <section>
-        <div>
-          <video controls width="100%">
-            <source src="https://player.vimeo.com/video/771916988?h=d7bec5dce4&background=1" />
-            {/* Add additional source tags for different video formats if needed */}
-          </video>
+        <div className={`${style.heroSection} container-fluid`}>
+          <div className="row mt-5" style={{ width: "100%" }}>
+            <div className='col-lg-12'>
+              <div className={`${style.heroRightSide}`}>
+                <span className={`${style.heroHeadOne}`}>CONNECTING BRANDS</span>
+                <span className={`${style.heroHeadTwo}`}>WITH GLOBAL</span>
+                <span className={`${style.heroHeadTwo}`}>AUDIENCES</span>
+              </div>
+            </div>
+            <div className='col-lg-12 mt-5'>
+              <div className="col-lg-6">
+                <div className={`${style.heroLeftSide}`}>
+                  <p>
+                    You have an idea, we have the talent and resources to turn it into a reality.
+                    Digital Gravity is a globally recognized digital agency elevating brands through
+                    ground-breaking user experience and disruptive digital products.
+                  </p>
+                  <Button text="Let's Talk"/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
-    // </NoSSR>
+    </>
   )
 }
 
