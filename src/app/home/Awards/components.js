b58e330d4e917771348f6d8e3f0fb211b48a0221
0318119 <professionalalexwilson@gmail.com>
2024-03-13 08:40:50 -0700

    some changes in rehman code by hamza waseem

--- src/app/home/Awards/components.js
+++ src/app/home/Awards/components.js
@@ -10,37 +10,39 @@ import forthImg from '../../assets/images/home/award-bagde-4.webp'
 
 const components = () => {
   return (
-    <div className={styles.container}>
-      <div className="row">
-        <div className={`col-lg-6 ${styles.leftContainer}`}>
-          <div className={styles.heading}>
-            <Text text="Awards" animation="fade-right" className={styles.Text} />
+    <section className={`${styles.bgColor}`}>
+      <div className={`container`}>
+        <div className="row">
+          <div className={`p-0 col-lg-6 ${styles.leftContainer}`}>
+            <div className={styles.heading}>
+              <Text text="Awards" animation="fade-right" className={styles.Text} />
+            </div>
+            <div className={styles.leftHeading}>
+              <h2>Digital Gravity, a
+                Synonym of Quality, Trust and Distinction</h2>
+              <p>Client satisfaction is one of our top priorities. Our consistency, dedication toward work, and constant innovation have won us several accolades in the last decade.
+                Hire web designers and web developers in Dubai from Digital Gravity to build highly responsive, scalable, and robust web applications with a touch of finesse.</p>
+            </div>
           </div>
-          <div className={styles.leftHeading}>
-            <h2>Digital Gravity, a
-              Synonym of Quality, Trust and Distinction</h2>
-            <p>Client satisfaction is one of our top priorities. Our consistency, dedication toward work, and constant innovation have won us several accolades in the last decade.
-              Hire web designers and web developers in Dubai from Digital Gravity to build highly responsive, scalable, and robust web applications with a touch of finesse.</p>
-          </div>
-        </div>
-        <div className={`col-lg-6 ${styles.rightContainer}`}>
-          <div className={styles.imagecontainer}>
-            <div className={styles.picture}>
-              <Image src={firstImg} alt="award-bagde-1" className={styles.slideImg} />
-            </div>
-            <div className={styles.picture}>
-              <Image src={SecondImg} alt="award-bagde-1"  className={styles.slideImg} />
-            </div>
-            <div className={styles.picture}>
-              <Image src={thirdImg} alt="award-bagde-1"  className={styles.slideImg} />
-            </div>
-            <div className={styles.picture}>
-              <Image src={forthImg} alt="award-bagde-1"  className={styles.slideImg} />
+          <div className={`p-0 col-lg-6 ${styles.rightContainer}`}>
+            <div className={styles.imagecontainer}>
+              <div className={styles.picture}>
+                <Image src={firstImg} alt="award-bagde-1" className={styles.slideImg} />
+              </div>
+              <div className={styles.picture}>
+                <Image src={SecondImg} alt="award-bagde-1" className={styles.slideImg} />
+              </div>
+              <div className={styles.picture}>
+                <Image src={thirdImg} alt="award-bagde-1" className={styles.slideImg} />
+              </div>
+              <div className={styles.picture}>
+                <Image src={forthImg} alt="award-bagde-1" className={styles.slideImg} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
--- src/app/home/SocialJoin/component.js
+++ src/app/home/SocialJoin/component.js
@@ -8,58 +8,58 @@ import { FaLinkedin } from "react-icons/fa6";
 
 const component = () => {
     return (
-        <div className={`container ${styles.container}`}>
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className={styles.heading}>
-                        <p>GET SOCIAL</p>
-                        <span>Join the conversation</span>
-                        <div className={styles.socialIcon}>
-                            <FaXTwitter className={styles.Icon} />
-                            <FaFacebookF className={styles.Icon} />
-                            <FaInstagram className={styles.Icon} />
-                            <FaYoutube className={styles.Icon} />
-                            <FaLinkedin className={styles.Icon} />
+        <section style={{background: "#F8F8F8"}}>
+            <div className={`container ${styles.container}`}>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className={styles.heading}>
+                            <p>GET SOCIAL</p>
+                            <span>Join the conversation</span>
+                            <div className={styles.socialIcon}>
+                                <FaXTwitter className={styles.Icon} />
+                                <FaFacebookF className={styles.Icon} />
+                                <FaInstagram className={styles.Icon} />
+                                <FaYoutube className={styles.Icon} />
+                                <FaLinkedin className={styles.Icon} />
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className={`row mt-3 ${styles.socialrow}`}>
+                    <div className={`col-lg-6 ${styles.Youtube}`}>
+                        <FaYoutube className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.facebook}`}>
+                        <FaFacebookF className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.facebook2}`}>
+                        <FaFacebookF className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.instagram}`}>
+                        <FaInstagram className={styles.YoutubeIcon} />
+                    </div>
+
+                    <div className={`col-lg-2 ${styles.twitter}`}>
+                        <FaXTwitter className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.instagram2}`}>
+                        <FaInstagram className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.facebook3}`}>
+                        <FaFacebookF className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.instagram3}`}>
+                        <FaInstagram className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.facebook4}`}>
+                        <FaInstagram className={styles.YoutubeIcon} />
+                    </div>
+                    <div className={`col-lg-2 ${styles.instagram}`}>
+                        <FaInstagram className={styles.YoutubeIcon} />
+                    </div>
+                </div>
             </div>
-            <div className={`row mt-3 ${styles.socialrow}`}>
-                <div className={`col-lg-6 ${styles.Youtube}`}>
-                    <FaYoutube className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.facebook}`}>
-                    <FaFacebookF className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.facebook2}`}>
-                    <FaFacebookF className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.instagram}`}>
-                    <FaInstagram className={styles.YoutubeIcon} />
-                </div>
-              
-                <div className={`col-lg-2 ${styles.twitter}`}>
-                    <FaXTwitter className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.instagram2}`}>
-                    <FaInstagram className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.facebook3}`}>
-                    <FaFacebookF className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.instagram3}`}>
-                    <FaInstagram className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.facebook4}`}>
-                    <FaInstagram className={styles.YoutubeIcon} />
-                </div>
-                <div className={`col-lg-2 ${styles.instagram}`}>
-                    <FaInstagram className={styles.YoutubeIcon} />
-                </div>
-            </div>
-     
-            
-        </div>
+        </section>
     )
 }
 
