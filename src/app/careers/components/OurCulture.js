d6d8a2c41c772da55304d282f63b896d9a539a44
0318119 <professionalalexwilson@gmail.com>
2024-03-15 16:12:15 -0700

    hanmzA

--- src/app/careers/components/OurCulture.js
+++ src/app/careers/components/OurCulture.js
@@ -5,33 +5,33 @@ import FESTIVTIY from "../../assets/images/OurCulture/Festivity.jpg"
 import TEAMWORK from "../../assets/images/OurCulture/Teamwork.jpg"
 import CAMARADERIE from "../../assets/images/OurCulture/Camaraderie.jpg"
 import IMAGINATION from "../../assets/images/OurCulture/Imagination.jpg"
-
+import Text from '../../component/text/index'
 
 const OurCulture = () => {
     return (
-        <div className={`${styles.ourculturesection}container-fluid `}>
+        <div className={`${styles.ourculturesection} container-fluid `}>
             <div className="row mt-5">
-                <div className="col-5 p-0">
+                <div className="col-lg-5 p-0">
                     <div className={`${styles.leftourculture}`}>
-                    <h1 className={styles.fadeRight}>Our Culture</h1>
+                    <Text text="Our Culture" animation="fade-right" />
                         <h2>OPPORTUNITIES <br /> DON'T HAPPEN, <br /> YOU CREATE THEM</h2>
                     </div>
                 </div>
-                <div className="col-7 p-0">
+                <div className="col-lg-7 p-0">
                     <div className={`row ${styles.imageRow}`}>
                         <div className={`col-6 p-0 ${styles.imageColumn}`}>
-                            <div className={`card ${styles.imageCard}`}>
+                            <div className={`card`}>
                                 <Image src={FESTIVTIY} className={`${styles.image}`} alt="Image 1" />
                             </div>
-                            <div className={`card ${styles.imageCard}`}>
+                            <div className={`card`}>
                                 <Image src={CAMARADERIE} className={`${styles.image}`} alt="Image 3" />
                             </div>
                         </div>
                         <div className={`col-6 p-0 ${styles.imageColumn}`}>
-                            <div className={`card ${styles.imageCard}`}>
+                            <div className={`card`}>
                                 <Image src={TEAMWORK} className={`${styles.image}`} alt="Image 2" />
                             </div>
-                            <div className={`card ${styles.imageCard}`}>
+                            <div className={`card`}>
                                 <Image src={IMAGINATION} className={`${styles.image}`} alt="Image 4" />
                             </div>
                         </div>
--- src/app/careers/page.js
+++ src/app/careers/page.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import Aboutbanner from '../about/components/Aboutbanner';
-import CareersBanners from '../careers/components/CareersBanners';
-import Cursor from "../component/cursor/cursor";
-import Header from "../component/Header/component"
-import CurrentOpenings from './components/CurrentOpenings';
-import Employeessay from './components/Employeessay';
-import OurCulture from './components/OurCulture';
+  import React from 'react'
+  import Aboutbanner from '../about/components/Aboutbanner';
+  import CareersBanners from '../careers/components/CareersBanners';
+  import Cursor from "../component/cursor/cursor";
+  import Header from "../component/Header/component"
+  import CurrentOpenings from './components/CurrentOpenings';
+  import Employeessay from './components/Employeessay';
+  import OurCulture from './components/OurCulture';
+  import Footer from '../component/Footer/component';
 
 
-const Page = () => {
-  return (
-    <>
-      <Cursor />
-      <Header />
-      <Aboutbanner />
-      <CareersBanners />
-      <CurrentOpenings />
-      <OurCulture />
-      <Employeessay/> 
-    </>
-  );
-};
+  const Page = () => {
+    return (
+      <>
+        <Cursor />
+        <Header />
+        <Aboutbanner />
+        <CareersBanners />
+        <CurrentOpenings />
+        <OurCulture />
+        <Employeessay/> 
+        <Footer />
+      </>
+    );
+  };
 
-export default Page;
+  export default Page;
