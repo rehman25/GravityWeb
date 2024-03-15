3c556c7680e96f12cdc34fc3957267a322539662
abdullaahsaeed <maddaabdullah@gmail.com>
2024-03-15 11:50:20 +0500

    abdullah

--- src/app/about/components/Aboutbanner.js
+++ src/app/about/components/Aboutbanner.js
@@ -8,7 +8,7 @@ const Aboutbanner = () => {
       <section>
         <div className={`${styles.banneraboutsection} `}>
           <div className={`row mt-5 ${styles.bannertex}`}>
-            <div className="col-lg-6 col-md-6 col-sm-6">
+            <div className="col-lg-6 col-md-12 col-sm-12">
               <div className={`${styles.bannertextleftside}`}>
                 <span className={`${styles.borderdesign}`}></span>
                 <h6>Digital Marketing Agency In Dubai</h6>
@@ -19,7 +19,7 @@ const Aboutbanner = () => {
                 </h1>
               </div>
             </div>
-            <div className="col-md-6 col-sm-6 mt-4">
+            <div className="col-lg-6 col-md-12 col-sm-12 mt-4">
               <div className={`${styles.bannertextrightside}`}>
                 <span>
                   Digital Gravity is an accomplished digital transformation
--- src/app/careers/page.js
+++ src/app/careers/page.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Aboutbanner from '../about/components/Aboutbanner';
-import CareersBanners from '../careers/components/Careersbanners';
+import CareersBanners from '../careers/components/CareersBanners';
 import Cursor from "../component/cursor/cursor";
 import Header from "../component/Header/component"
+import CurrentOpenings from './components/CurrentOpenings';
+import Employeessay from './components/Employeessay';
+import OurCulture from './components/OurCulture';
+
 
 const Page = () => {
   return (
     <>
       <Cursor />
-      <Header/>
+      <Header />
       <Aboutbanner />
       <CareersBanners />
+      <CurrentOpenings />
+      <OurCulture />
+      <Employeessay/> 
     </>
   );
 };
