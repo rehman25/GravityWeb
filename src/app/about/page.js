888448b8aea53d0e2980830f3a347ddfd4de8542
Mustufa <mustufakhanofficial97@gmail.com>
2024-03-20 09:04:14 +0500

    about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,11 +6,12 @@ import About_subheading from '../about/components/about_subheading'
 import AboutSlider from './components/AboutSlider';
 import Expertise from "../about/components/Expertise";
 import Cursor from "../component/cursor/cursor";
-
+import Header from "../component/Header/component";
 const Page = () => {
   return (
     <>
       <Cursor />
+      <Header />
       <Aboutbanner />
       <Bannerdown />
       <AboutCarts />
