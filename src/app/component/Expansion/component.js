97bbc8d4b98e5b898b648a1c85645ab690c4ff9f
abdullaahsaeed <maddaabdullah@gmail.com>
2024-03-22 11:43:10 +0500

    abdullah

--- src/app/component/Expansion/component.js
+++ src/app/component/Expansion/component.js
@@ -16,7 +16,7 @@ const components = () => {
           <div className="col-lg-6 p-0">
             <div className={`${styles.Expansionsectionleft} p-5`}>
               <h3>Now Epanding To Multiple <br />
-                Cities Across USA, GCC Region & Asia</h3>
+                Cities Across <span className={`${styles.Expansionheadersub}`}>USA, GCC Region & Asia</span></h3>
               <Image src={Map} alt='example' />
               <div className={`${styles.CitiesInfo}`}>
                 <div className={`${styles.USAIH5}`}>
@@ -52,25 +52,25 @@ const components = () => {
               </div>
             </div>
           </div>
-          <div className={`col-lg-6 mt-5 ${styles.Expansionsectionright}`}>
+          <div className={`col-lg-6 mt-3 ${styles.Expansionsectionright}`}>
             <div className={styles.FormContainer}>
               <form action="" className={styles.ContactForm}>
-                <h3>Got an Idea? <br /> Let’s get started </h3>
+                <h3>Got an Idea? <br />  <span className={`${styles.Expansionheaderrightsub}`}> Let’s get started </span> </h3>
                 <h4> Fill out the Form </h4>
                 <div className={`row ${styles.row}`}>
                   <div className={`${styles.Formgroup} col-md-6`}>
                     <label htmlFor="" className={styles.label}>Your Name*</label>
-                    <input type="text" className={` ${styles.Input}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your Name'/>
+                    <input type="text" className={` ${styles.Input}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your Name' />
                   </div>
                   <div className={`${styles.Formgroup} col-md-6`}>
                     <label htmlFor="" className={styles.label}>Your Email*</label>
-                    <input type="text" className={` ${styles.Input}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your Email'/>
+                    <input type="text" className={` ${styles.Input}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your Email' />
                   </div>
                 </div>
                 <div className={`row ${styles.row}`}>
                   <div className={`${styles.Formgroup} col-md-6`}>
                     <label htmlFor="" className={styles.label}>Your Phone Number*</label>
-                    <input type="text" className={` ${styles.Input}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your phone number'/>
+                    <input type="text" className={` ${styles.Input}`} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your phone number' />
                   </div>
                   <div className={`${styles.Formgroup} col-md-6`}>
                     <label htmlFor="" className={styles.label}>Budget</label>
@@ -83,7 +83,7 @@ const components = () => {
                 </div>
                 <div className={styles.Formgrouptextarea}>
                   <label htmlFor="" className={styles.label}>Your Message</label>
-                  <textarea name="" id="" cols="120" rows="5" className={styles.textarea} placeholder='Type Here' />
+                  <textarea name="" id="" cols="60" rows="5" className={styles.textarea} placeholder='Type Here' />
                 </div>
                 <div className={styles.button}>
                   <span className={styles.sendtext}>Send</span>
