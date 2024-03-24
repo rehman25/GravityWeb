d00e0ffd0473f5f2fefd5afe17151570ba1933f4
abdullaahsaeed <maddaabdullah@gmail.com>
2024-03-24 08:01:12 +0500

    'abdullah'

--- src/app/component/ServiceTechDevelopment/component.js
+++ src/app/component/ServiceTechDevelopment/component.js
@@ -1,6 +1,11 @@
-import React from "react";
+'use client'
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "../../styles/about.module.css";
+import styles from "../../component/ServiceTechDevelopment/ServiceTechDevelopment.module.css";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import Image from "next/image";
 import one from "../../assets/images/ServicTechDevelopment/technology-icon-1.svg";
 import two from "../../assets/images/ServicTechDevelopment/technology-icon-2.svg";
@@ -25,20 +30,56 @@ import twenty from "../../assets/images/ServicTechDevelopment/technology-icon-20
 import twentyone from "../../assets/images/ServicTechDevelopment/technology-icon-21.svg";
 import twentytwo from "../../assets/images/ServicTechDevelopment/technology-icon-22.svg";
 import twentythree from "../../assets/images/ServicTechDevelopment/technology-icon-23.svg";
+import Resturant from "../../assets/images/ServicTechDevelopment/service-development8.webp"
+import RealEstate from "../../assets/images/ServicTechDevelopment/service-development7.webp"
+import Technology from "../../assets/images/ServicTechDevelopment/service-development6.webp"
+import BusinessConsulting from "../../assets/images/ServicTechDevelopment/service-development5.webp"
+import Education from "../../assets/images/ServicTechDevelopment/service-development4.webp"
+import Manufacturing from "../../assets/images/ServicTechDevelopment/service-development3.webp"
+import HealthCare from "../../assets/images/ServicTechDevelopment/service-development2.webp"
+import Automotive from "../../assets/images/ServicTechDevelopment/service-development1.webp"
+
 
 const component = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        arrows: true,
+        prevArrow: <FaArrowLeft />,
+        nextArrow: <FaArrowRight />,
+        beforeChange: (current, next) => setCurrentSlide(next)
+    };
+
+    const slides = [
+        { image: Resturant, text: "Restaurant" },
+        { image: RealEstate, text: "Real Estate" },
+        { image: Technology, text: "Technology" },
+        { image: BusinessConsulting, text: "Business Consulting" },
+        { image: Education, text: "Education" },
+        { image: Manufacturing, text: "Manufacturing" },
+        { image: HealthCare, text: "Health Care" },
+        { image: Automotive, text: "Automotive" }
+    ];
     return (
         <>
             <section style={{ overflowX: "clip", marginTop: "50px" }}>
-                <div className="bg-info text-white py-5">
+                <div className={`${styles.ServiceTechDevelopmentsection}`}>
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
-                                <div>
-                                    <h1 className="text-center mb-4">
+                                <div className={`${styles.ServiceTechDevelopmentupper}`}>
+                                    <h1>
                                         Web development technology stack
                                     </h1>
-                                    <p className="text-center mb-5">
+                                    <p>
                                         We are experts with highly innovative and cutting-edge digital technology, built exclusively around your business vision.
                                     </p>
                                 </div>
@@ -257,9 +298,60 @@ const component = () => {
                                 </div>
                             </div>
                         </div>
+                        <div className="row mt-5">
+                            <div className={`${styles.ServiceTechDevelopmentdown}`}>
+                                <h2>We help a diverse clientele build result-driven Web Development </h2>
+                            </div>
+                            <div className="col-lg-7 my-5">
+                                <Slider {...settings}>
+                                    {slides.map((slide, index) => (
+                                        <div key={index} className={`${styles.Imageslider}`}>
+                                            <div className={styles.SliderImg}>
+                                                <Image src={slide.image} alt={slide.text} width={300} height={200} />
+                                            </div>
+                                            <span>{slide.text}</span>
+                                        </div>
+                                    ))}
+                                </Slider>
+                            </div>
+                            <div className="col-lg-5 my-5">
+                                <div className={`${styles.ImagesliderContent}`}>
+                                    <div className="SliderContent">
+                                        <h4>{slides[currentSlide].text}</h4>
+                                        {slides[currentSlide].text === "Restaurant" && (
+                                            <p>Food businesses and restaurant owners know Digital Gravity for its branding services. We design their websites, establish a powerful social media presence, and bring them new customers consistently.</p>
+                                        )}
+                                        {slides[currentSlide].text === "Real Estate" && (
+                                            <p>From website development to content marketing, Digital Gravity is the go-to company of famed Real Estate companies in the UAE. We take care of the techie details while they focus on the bottom line.</p>
+                                        )}
+                                        {slides[currentSlide].text === "Technology" && (
+                                            <p>As a full-service digital agency, we love working with IT start-ups. Numerous IT service providers have hired us to top off the exciting web and mobile projects, while some of them use our creative services to enhance digital visibility. </p>
+                                        )}
+                                        {slides[currentSlide].text === "Business Consulting" && (
+                                            <p>We evaluate our level of success based on your satisfaction. With our creative services, we have helped several Business Consulting services in Dubai reach new heights and grow their client base.</p>
+                                        )}
+                                        {slides[currentSlide].text === "Education" && (
+                                            <p>Our expert web developers have designed beautiful education websites, mobile apps, and eLearning portals for our clients. Our clients’ substantial growth through these channels reflects the quality of our work. </p>
+                                        )}
+                                        {slides[currentSlide].text === "Manufactuing" && (
+                                            <p> Many of our clients come from the manufacturing industry. Besides web support, all of them require assistance in the marketing of their products and services.   </p>
+                                        )}
+                                        {slides[currentSlide].text === "Health Care" && (
+                                            <p>Our team is responsible for website maintenance, support, and search engine optimization of some of the leading healthcare providers in Dubai including Capital Health Screening Service. </p>
+                                        )}
+                                        {slides[currentSlide].text === "Automotive" && (
+                                            <p>Our team is responsible for website maintenance, support, and search engine optimization of some of the leading healthcare providers in Dubai including Capital Health Screening Service. </p>
+                                        )}
+                                        <div className={styles.ArrowIcons}>
+                                            <FaArrowLeft className={styles.LeftArrow} />
+                                            <FaArrowRight className={styles.RightArrow} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
             </section>
         </>
     );
@@ -267,3 +359,5 @@ const component = () => {
 
 
 export default component;
+
+
